00d5fa699e67dfd04648f97083f21fece7e96d68
sivabharathi <sivabharathi.subramanian@gmail.com>
2021-08-20 16:50:45 +0530

    initial check-in

---
+++ src/main/java/com/siva/devops/siva_maven/App.java
@@ -0,0 +1,13 @@
+package com.siva.devops.siva_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from siva!" );
+    }
+}
